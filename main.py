cbd57094cef1270c79e7863135510761ed03919e
Azane Azariah Kofi <117452335+Briggs305@users.noreply.github.com>
2024-05-27 16:10:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ secret_num = random.randint(1, 15)
 while True:
     # Get player's guess
 
-    for guessesTaken in range(1,7):
-        print('Lets start by taking a guess.')
+    for guessesTaken in range(1,9):
+
         guess = int(input())
+
         if guess > secret_num:
             print('guess is too high.')
         elif guess < secret_num:
             print ('guess is too low.')
-
-    if guess == secret_num:
+        elif guess == secret_num:
+            print('Good job' + str  (Myname) + ' You guessed my number in ' + str(guessesTaken) + ' guesses!')
+    if guessesTaken == secret_num:
         break  # This condition is for the correct guess
-        print('Good job' + str (Myname)  + ' You guessed my number in ' + str ( guessesTaken ) + ' guesses!')
+        print('Good job' + str  (Myname)  + ' You guessed my number in ' + str ( guessesTaken ) + ' guesses!')
     else:
         print( "Sorry that's not correct," + str (Myname) + " the secret number is " + str (secret_num) + " thank you for playing!")
 
